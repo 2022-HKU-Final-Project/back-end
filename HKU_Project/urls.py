864337d8aa91b7389fc405e78f0319e37c33c41e
yuhao101 <yuhao101@connect.hku.hk>
2022-06-21 21:07:40 +0800

    update

--- HKU_Project/urls.py
+++ HKU_Project/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from HKU_Project import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
