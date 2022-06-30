1e4c6596517f75ac44b174b47d2a116a7c3d3819
yuhao101 <yuhao101@connect.hku.hk>
2022-06-30 23:12:49 +0800

    update home

--- jobRecommend/views.py
+++ jobRecommend/views.py
@@ -10,10 +10,14 @@ def products(request):
 
 
 def jobs(request):
-    search_key = json.loads(request.body)['keyword']
 
-    # search_key 是 home 搜索内容, 如果为空则说明是首页直接跳转，无输入内容
+    # search_key 是 home 搜索内容，如果为空则说明是首页直接跳转，无输入内容
+    search_key = json.loads(request.body)['keyword']
     print(search_key)
+
+    # job_category_id_list 是 home 下方职位类别跳转，如果为空则说明不是点击下方职位类别跳转，直接忽略
+    print(json.loads(request.body)['job_category_id_list'])
+
 
     test = {
         'count': 1,
@@ -47,6 +51,21 @@ def jobType(request):
         'job_type_list':[1,2,3]
     }
     return HttpResponse(json.dumps(test, ensure_ascii=False))
+
+
+def job_categories(request):
+    data = json.load(open('./Data/data.json', 'r'))['jobCategories']
+    return HttpResponse(json.dumps(data, ensure_ascii=False))
+
+
+def staff_stories(request):
+    data = json.load(open('./Data/data.json', 'r'))['staffStories']
+    return HttpResponse(json.dumps(data, ensure_ascii=False))
+
+
+def byte_standards(request):
+    data = json.load(open('./Data/data.json', 'r'))['byteStandards']
+    return HttpResponse(json.dumps(data, ensure_ascii=False))
 
 
 def recommend(request):
